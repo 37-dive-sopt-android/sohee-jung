a57a95b7a9ae7ccfbaa95cd7250724ebb19dad9f
sohee <144779368+sohee6989@users.noreply.github.com>
2025-10-15 16:34:19 +0900

    [feat/#2]: 유효성 검사 실패 시 에러 메세지 함수 구현

--- app/src/main/java/com/sopt/dive/core/util/DiveSoptValidator.kt
+++ app/src/main/java/com/sopt/dive/core/util/DiveSoptValidator.kt
@@ -1,9 +1,14 @@
 package com.sopt.dive.core.util
 
+import com.sopt.dive.core.util.DiveSoptValidator.isMbtiFormat
+import com.sopt.dive.core.util.DiveSoptValidator.isNicknameFormat
+import com.sopt.dive.core.util.DiveSoptValidator.isPasswordFormat
+import com.sopt.dive.core.util.DiveSoptValidator.isUserIdFormat
+
 object DiveSoptValidator {
-    private val userIdPattern = "^{6,12}$".toRegex()
-    private val passwordPattern = "^[a-zA-Z0-9]{8,12}$".toRegex()
-    private val nicknamePattern = "^\\S+$".toRegex()
+    private val userIdPattern = "^[ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9]{6,12}$".toRegex()
+    private val passwordPattern = "^[A-Za-z\\d~!@#$%^&*]{8,12}$".toRegex()
+    private val nicknamePattern = "^[ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9]\\S+$".toRegex()
     private val mbtiPattern = "^[IE][NS][FT][JP]$".toRegex(option = RegexOption.IGNORE_CASE)
 
     fun isUserIdFormat(input: String): Boolean {
@@ -21,4 +26,27 @@ object DiveSoptValidator {
     fun isMbtiFormat(input: String): Boolean {
         return mbtiPattern.matches(input)
     }
+}
+
+fun validateMessage(
+    userId: String,
+    password: String,
+    nickname: String,
+    mbti: String
+): List<String> {
+    val errors = mutableListOf<String>()
+
+    if (!isUserIdFormat(userId)) {
+        errors += "ID 형식이 올바르지 않습니다. (영문/숫자/한글 6~12자)"
+    }
+    if (!isPasswordFormat(password)) {
+        errors += "비밀번호 형식이 올바르지 않습니다. (영문/숫자/특수문자 조합 8~16자)"
+    }
+    if (!isNicknameFormat(nickname)) {
+        errors += "닉네임 형식이 올바르지 않습니다. (공백 불가)"
+    }
+    if (!isMbtiFormat(mbti)) {
+        errors += "MBTI 형식이 올바르지 않습니다. (예: INTJ)"
+    }
+    return errors
 }
